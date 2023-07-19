8b25240f9a0d7129868b5db454d132932233ffc0
julianazanelatto <julianazanelatto@gmail.com>
2023-07-19 14:05:16 -0300

    Creating the connections

--- Tranformation/documents_creation.py
+++ Tranformation/documents_creation.py
@@ -4,6 +4,7 @@
     :return posts
 """
 import pandas as pd
+import json
 
 def transformig_data(data):
 
@@ -16,15 +17,38 @@ def transformig_data(data):
     ids = df['id_order'].tolist()
 
     for code in ids:
-        (df.loc[lambda df: df['id_order'] == code])
+        sub_df = (df.loc[lambda df: df['id_order'] == code])
+        documents = None
+        for index in range(0, len(sub_df)):
+            print(sub_df['id_order'][index])
 
+            if documents is None:
+                documents = {
+                        "id_pedido": sub_df['id_order'][index],
+                        "id_customer": sub_df['id_customer'][index],
+                        # "id_vendedor": 1350,
+                        "local": {
+                            "city": sub_df['city'][index],
+                            "state": sub_df['state'][index],
+                            "country":  sub_df['country'][index]
+                        },
+                        "order_date": sub_df['order_date'][index],
+                        "order_status":  sub_df['status'][index],
+                        "products": [
+                            {
+                                "id_product": 123,
+                                "name": "",
+                                "category": "",
+                                "price": 12.1
+                            },
+                        ],
+                }
+            else:
+                # adiciona os demais produtos
+                pass
 
-        documents = {
-            "author": "Mike",
-            "text": "My first mongodb application based on python",
-            "tags": ["mongodb", "python3", "pymongo"],
-            "date": ""
-        }
+            print(documents['products'])
+            input()
 
     return data
 
